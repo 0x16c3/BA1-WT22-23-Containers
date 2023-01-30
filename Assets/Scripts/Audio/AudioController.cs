19e9be1dc7b3723530097455be49a03de0f865ea
Pat173 <juan.ortiz1734@gmail.com>
2023-01-30 23:57:51 +0100

    feat: Audio Assets added onto the project
    Still missing the implementation in their respective places, working on that next.

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -5,33 +6,70 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    AudioSource audioSource;
-
-    private void Start()
-    { 
-        gameObject.SetActive(false);
-    }
-
-    private void OnEnable()
+    public Sounds[] Sounds;
+    public static AudioController instance;
+    private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
-        // Wait for the audio to finish playing before deactivating the game object
-        StartCoroutine(DeactivateAfterAudioFinish(audioSource.clip.length));
-    }
-
-    private void Update()
-    {
-        if (gameObject.tag == "AudioMusic"){
-            audioSource.volume = AudioVolumeController.MusicVolume / 100;
+        if (instance == null)
+        {
+            instance = this;
         }
-        else{
-            audioSource.volume = AudioVolumeController.AmbianceVolume / 100;
-        }  
+        else
+        {
+            Destroy(instance);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sounds audio in Sounds)
+        {
+            audio.AudioSource = gameObject.AddComponent<AudioSource>();
+            audio.AudioSource.clip = audio.Clip;
+
+            audio.AudioSource.volume = audio.Volume;
+            audio.AudioSource.loop = audio.Loop;
+        }
     }
 
-    IEnumerator DeactivateAfterAudioFinish(float delay)
+    public void PlayAudio(string name)
     {
-        yield return new WaitForSeconds(delay);
-        gameObject.SetActive(false);
+        Sounds audio = Array.Find(Sounds, Sounds => Sounds.Name == name);
+        if (audio == null) 
+        {
+            Debug.LogWarning("Sound of name " + name + " not found");
+            return;
+        }
+        // set the volume to the one set in the menu by the player depending on the AudioType variable
+        audio.AudioSource.Play();
     }
+
+    /*AudioSource audioSource;
+
+private void Start()
+{ 
+    gameObject.SetActive(false);
+}
+
+private void OnEnable()
+{
+    audioSource = GetComponent<AudioSource>();
+    // Wait for the audio to finish playing before deactivating the game object
+    StartCoroutine(DeactivateAfterAudioFinish(audioSource.clip.length));
+}
+
+ private void Update()
+{
+    if (gameObject.tag == "AudioMusic"){
+        audioSource.volume = AudioVolumeController.MusicVolume / 100;
+    }
+    else{
+        audioSource.volume = AudioVolumeController.AmbianceVolume / 100;
+    }  
+}
+
+IEnumerator DeactivateAfterAudioFinish(float delay)
+{
+    yield return new WaitForSeconds(delay);
+    gameObject.SetActive(false);
+}*/
 }
